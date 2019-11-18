7643993895ef867e2d4bc14a32fac752ad9d3fa9
Erik Sandoval <erik.sandoval@icloud.com>
2019-11-18 10:06:47 -0800

    changed log in state to true to avoid login screen.

--- instagram/src/authentication/withAuthenticate.js
+++ instagram/src/authentication/withAuthenticate.js
@@ -1,37 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const withAuthenticate = App => LoginPage =>
-    class extends React.Component {
-        constructor() {
-            super();
-            this.state = {
-                loggedIn: false,
-                username: '',
-                password: ''
-            }
-        }
-
-        handleChange = event => {
-            console.log(event.target.name)
-            this.setState({
-                [event.target.name]: event.target.value
-            })
-        }
-
-        toggleLogin = event => {
-            event.preventDefault();
-            localStorage.setItem('username', this.state.username)
-            localStorage.setItem('password', this.state.password)
-            document.location.reload();
-        }
-
-        render() {
-            if (localStorage.getItem('username') && localStorage.getItem('password')) {
-                return <App />
-            } else {
-                return <LoginPage toggle={this.toggleLogin} handleChange={this.handleChange} />
-            }
-        }
+  class extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        loggedIn: true,
+        username: "",
+        password: ""
+      };
     }
 
-export default withAuthenticate
+    handleChange = event => {
+      console.log(event.target.name);
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    };
+
+    toggleLogin = event => {
+      event.preventDefault();
+      localStorage.setItem("username", this.state.username);
+      localStorage.setItem("password", this.state.password);
+      document.location.reload();
+    };
+
+    render() {
+      if (
+        localStorage.getItem("username") &&
+        localStorage.getItem("password")
+      ) {
+        return <App />;
+      } else {
+        return (
+          <LoginPage
+            toggle={this.toggleLogin}
+            handleChange={this.handleChange}
+          />
+        );
+      }
+    }
+  };
+
+export default withAuthenticate;
